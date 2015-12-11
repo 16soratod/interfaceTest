e4aa240b386150469998481922405c15e6b8f6a0
16soratod <sdoken@stuy.edu>
2015-12-11 10:35:58 -0500

    add cylinder

---
+++ Cylinder.java
@@ -0,0 +1,20 @@
+public class Cylinder extends Circle implements Volume{
+
+    private double height;
+
+    public Cylinder(double radius, double height,String name){
+	super(radius,name);
+	this.height = height;
+    }
+
+    public double getHeight(){
+	return height;
+    }
+
+    public String toString(){
+	return "Cylinder"  + getName() + " that is " + getArea() + " and height " + getHeight();
+    }
+    public double getVolume(){
+	return getArea() * height;
+    }
+}
